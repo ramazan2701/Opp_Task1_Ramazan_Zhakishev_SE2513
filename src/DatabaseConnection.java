fe7e79e64490754caea367e3888b5662108e5505
ramazan2701 <ramazanzakishev@gmail.com>
2026-01-17 10:51:54 +0500

    added db

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -4,7 +4,8 @@ import java.sql.SQLException;
 
 public final class DatabaseConnection {
 
-    private static final String URL = "jdbc:postgresql://localhost:5432/warehouse_db";
+    private static final String URL =
+            "jdbc:postgresql://localhost:5432/warehouse_db";
     private static final String USER = "postgres";
     private static final String PASSWORD = "postgres";
 
